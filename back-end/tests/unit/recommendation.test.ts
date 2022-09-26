687fddc43cd5eb30316d26abcb8ceb6e321e4b3a
Lucas Palharini <lucas230991@hotmail.com>
2022-09-26 06:53:28 -0300

    feat: create unit tests

---
+++ back-end/tests/unit/recommendation.test.ts
@@ -0,0 +1,13 @@
+import { prisma } from '../../src/database';
+import { jest } from "@jest/globals";
+
+import { Recommendation } from '@prisma/client';
+
+import * as recommendationRepository from '../../src/repositories/recommendationRepository';
+import * as recommendationService from '../../src/services/recommendationsService';
+
+describe('Testes unitários do recommendation Service', () => {
+    it.todo('', async () => {
+    });
+    
+  });
