48cbb21eec946df8c46431719bfdb4e07a37c18c
Lucas Palharini <lucas230991@hotmail.com>
2022-09-26 14:05:05 -0300

    fix: add disconnect() to integration

--- back-end/tests/integration/recommendation.test.ts
+++ back-end/tests/integration/recommendation.test.ts
@@ -1,16 +1,20 @@
 import app from '../../src/app';
 import supertest from 'supertest';
-import { deleteTable } from '../factories/scenarioFactory';
+import { deleteTable, disconnect } from '../factories/scenarioFactory';
 
 const server = supertest(app);
 
 beforeEach(async () => {
     await deleteTable();
-})
+});
+
+afterAll(async () => {
+    await disconnect();
+});
 
 describe("Teste em POST /recommendations", () => {
 
-})
+});
 describe("Teste em GET /recommendations", () => {
-    
-})
+
+});
