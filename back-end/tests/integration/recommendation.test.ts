b475e04547a82a87b10c499499540db943f2d49a
Lucas Palharini <lucas230991@hotmail.com>
2022-09-26 15:07:24 -0300

    feat:  post fails recommendation test

--- back-end/tests/integration/recommendation.test.ts
+++ back-end/tests/integration/recommendation.test.ts
@@ -1,7 +1,7 @@
 import app from '../../src/app';
 import supertest from 'supertest';
 import { deleteTable, disconnect } from '../factories/scenarioFactory';
-import { __newRecommendation } from '../factories/recommendationFactory';
+import __createRecommendation, { __newRecommendation } from '../factories/recommendationFactory';
 
 const server = supertest(app);
 
@@ -14,12 +14,31 @@ afterAll(async () => {
 });
 
 describe("Teste em POST /recommendations", () => {
-    it("Teste de criar a roda de criação de recomendação", async () => {
+    it("Teste de criar a roda de criação de recomendação com sucesso", async () => {
         const recommendation = await __newRecommendation(); 
 
         const result = await server.post('/recommendations').send(recommendation);
 
         expect(result.status).toBe(201)
+    });
+
+    it("Teste de criar a roda de criação de recomendação com body errado", async () => {
+        const aux = await __createRecommendation(); 
+        const recommendation = {name: aux.name, youtubeLink: aux.youtubeLink};
+
+        const result = await server.post('/recommendations').send(recommendation);
+
+        expect(result.status).toBe(422)
+    });
+
+    it("Teste de criar a roda de criação de recomendação duplicado", async () => {
+        const recommendation = await __newRecommendation(); 
+
+        await server.post('/recommendations').send(recommendation);
+
+        const result = await server.post('/recommendations').send(recommendation);
+
+        expect(result.status).toBe(409)
     });
 });
 describe("Teste em GET /recommendations", () => {
